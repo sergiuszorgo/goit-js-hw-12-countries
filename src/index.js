682e91a42f3d19fd370def9c86540cbd44775ba9
sergiuszorgo <67199252+sergiuszorgo@users.noreply.github.com>
2020-12-26 18:09:06 +0200

    Накидываю файлы понемногу

---
+++ src/index.js
@@ -0,0 +1,2 @@
+import css from "./css/styles.css";
+import refs from "./js/refs.js";
---
+++ src/js/refs.js
@@ -0,0 +1,6 @@
+const refs = {
+    input: document.querySelector(".input"),
+    countryBlock: document.querySelector(".countryBlock"),
+    countryList: document.querySelector(".countryList")
+}
+export default refs;
